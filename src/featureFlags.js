629737f126b55a34251352c8dd92b89c76109f27
Adam Wathan <adam.wathan@gmail.com>
2020-10-04 13:03:03 -0400

    Better error handling for removed experimental features

--- src/featureFlags.js
+++ src/featureFlags.js
@@ -5,7 +5,6 @@ import log from './util/log'
 const featureFlags = {
   future: ['removeDeprecatedGapUtilities', 'purgeLayersByDefault'],
   experimental: [
-    'uniformColorPalette',
     'extendedSpacingScale',
     'defaultLineHeights',
     'extendedFontSizeScale',
@@ -15,6 +14,15 @@ const featureFlags = {
     'additionalBreakpoint',
     'redesignedColorPalette',
   ],
+  removed: {
+    uniformColorPalette: [
+      `The ${chalk.red(
+        'uniformColorPalette'
+      )} experiment has been removed in favor of a different color palette.`,
+      'If you would like to continue using this palette, you can copy it manually from here:',
+      'https://github.com/tailwindlabs/tailwindcss/blob/753925f72c61980fa6d7ba398b7e2a1ba0e4b438/src/flagged/uniformColorPalette.js',
+    ],
+  },
 }
 
 export function flagEnabled(config, flag) {
@@ -47,7 +55,26 @@ function futureFlagsAvailable(config) {
   return featureFlags.future.filter(flag => !_.has(config, ['future', flag]))
 }
 
+function removedFlagsEnabled(config) {
+  if (config.experimental === 'all') {
+    return false
+  }
+
+  return Object.keys(_.get(config, 'experimental', {})).filter(
+    flag => Object.keys(featureFlags.removed).includes(flag) && config.experimental[flag]
+  )
+}
+
 export function issueFlagNotices(config) {
+  if (removedFlagsEnabled(config).length > 0) {
+    removedFlagsEnabled(config).forEach(flag => {
+      log.error(featureFlags.removed[flag])
+    })
+
+    console.warn('')
+    throw new Error('Enabled experimental features have been removed.')
+  }
+
   if (process.env.JEST_WORKER_ID !== undefined) {
     return
   }
